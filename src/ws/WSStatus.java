67fd7dfb108a40f3351014a9d2c1c597c216b91c
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2018-12-04 06:59:44 -0200

    Update WSStatus.java

---
+++ src/ws/WSStatus.java
@@ -0,0 +1,37 @@
+package rtools;
+
+/**
+ * Retorna o status http da requisição
+ *
+ */
+public class WSStatus {
+
+    private Integer codigo;
+    private String descricao;
+
+    public WSStatus() {
+        this.codigo = null;
+        this.descricao = "";
+    }
+
+    public WSStatus(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
